5ac9e2e26a7e1f46559e33f7a5965fd8516e3f0e
plains1mple <up201806388@fe.up.pt>
2020-05-24 15:51:46 +0100

    on the banks

--- proj/src/ambient/MyCubeMap.js
+++ proj/src/ambient/MyCubeMap.js
@@ -70,7 +70,6 @@ class MyCubeMap extends CGFobject {
         this.top.apply();
         this.scene.pushMatrix();
         this.scene.translate(0, 0.5, 0);
-        this.scene.rotate(Math.PI, 0, 1, 0);
         this.scene.rotate(Math.PI/2, 1, 0, 0);
         this.quad.display();
         this.scene.popMatrix();
